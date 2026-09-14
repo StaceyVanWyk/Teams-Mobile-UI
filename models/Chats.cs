c776eee09e04f8af35fcc0a1e4a11e042d76cd18
Stacey Van Wyk <svanwyk722@gmail.com>
2026-09-14 10:55:47 +0200

    dev: added the properties so that one chat oject can hold four separate pieces of information rather than just a string

--- models/Chats.cs
+++ models/Chats.cs
@@ -4,5 +4,12 @@ public class Class1
 {
 	public Class1()
 	{
+		public string Name { get; set; 
+		public string Message { get; set;  }
+	    public string Time { get; set; }
+	    pubice string Image { get; set;  }
+	   
+	 
+	}
 	}
 }
